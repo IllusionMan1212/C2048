87646e657bbf909376951bde372aa42961f8a88a
IllusionMan1212 <hisham.abourgheba@gmail.com>
2023-09-02 00:06:08 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,8 @@
+int main(int argc, char *argv[]) {
+  // From scratch
+  // X11 for windowing
+  // OpenGL for rendering
+  // OpenAL for audio (maybe)
+  // Windows support when everything is fine on linux using winapi
+  return 0;
+}
